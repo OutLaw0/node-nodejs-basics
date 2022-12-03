742e53d04ea96492dd919f93282cb23926b3ca7d
Valentin Ak <vaakimov@gmail.com>
2022-12-03 18:19:42 +0300

    feat: add cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
+import { env } from 'process'
+
 const parseEnv = () => {
-    // Write your code here 
+  const rssKeys = Object.keys(env).filter(e => e.split("_")[0] === "RSS");
+  const print = rssKeys.map((key) => key + '=' + env[key]).join('; ')
+  console.log(print)
 };
 
 parseEnv();
