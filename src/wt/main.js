2446e1ad07d4e7c41b813d9850f84ea1a64ff323
Valentin Ak <vaakimov@gmail.com>
2022-12-06 00:30:46 +0300

    feat: add wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,49 @@
+import { returnPathAbs } from '../fs/utils.js';
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
+const workerPath = returnPathAbs('worker.js', import.meta.url)
+
 const performCalculations = async () => {
-    // Write your code here
+  let num = 10;
+  const cpuData = cpus();
+  // const resultData = [];
+  const workersPromiseResponses = await Promise.allSettled(cpuData.map(() => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(workerPath, {
+        workerData: num++
+      })
+
+      worker.on('message', resolve)
+      worker.on('error', reject)
+    })
+  }));
+  /*  worker.on('message', msg => {
+      resolve();
+      resultData.push({
+        status: 'resolved',
+        data: msg
+      });
+    })
+    worker.on('error', () => {
+      reject();
+      resultData.push({
+        status: 'error',
+        data: null
+      });
+    })
+  })
+}))*/
+
+  const resultData = workersPromiseResponses.map((resp) => {
+    return {
+      status: resp.status === 'fulfilled' ? 'resolved' : 'error',
+      data: resp.status === 'fulfilled' ? resp.value : null
+    }
+  });
+
+  console.log(resultData);
+  return resultData;
 };
 
 await performCalculations();
